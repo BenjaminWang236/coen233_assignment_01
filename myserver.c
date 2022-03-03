d9e54e254a3eed459c19774225378421aea6d4a3
BenjaminWang236 <ben.lynbrook@gmail.com>
2022-03-02 17:47:10 -0800

    baseline myserver, awaiting custom protocol

--- myserver.c
+++ myserver.c
@@ -8,9 +8,11 @@
  *
  * @copyright Copyright (c) 2022
  * @source: https://www.linuxhowtos.org/C_C++/socket.htm
+ * @source: https://www.geeksforgeeks.org/udp-server-client-implementation-c/
  *
  */
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -19,6 +21,10 @@
 #include <string.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAXLINE 1024
 
 /**
  * @brief Error function
@@ -28,11 +34,11 @@
 void error(const char *msg)
 {
     perror(msg);
-    exit(0);
+    exit(EXIT_FAILURE);
 }
 
 /**
- * @brief Main function
+ * @brief Main function (Driver code)
  *
  * @param argc number of arguments
  * @param argv arguments
@@ -40,40 +46,57 @@ void error(const char *msg)
  */
 int main(int argc, char *argv[])
 {
+    // Define variables
     int sock, length, n;
-    socklen_t fromlen;
-    struct sockaddr_in server;
-    struct sockaddr_in from;
-    char buf[1024];
+    socklen_t clientlen;
+    struct sockaddr_in server, client;
+    char buf[MAXLINE];
+    char *hello = "Hello from server";
 
+    // Checking if port number is provided
     if (argc < 2)
-    {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(0);
-    }
+        error("ERROR: no port provided");
 
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0)
-        error("Opening socket");
+    // Creating socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        error("ERROR: Opening socket");
+
+    // Filling server information
     length = sizeof(server);
-    bzero(&server, length);
+    bzero(&server, length); // memset(&servaddr, 0, length);
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(atoi(argv[1]));
+    // server.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
     if (bind(sock, (struct sockaddr *)&server, length) < 0)
-        error("binding");
-    fromlen = sizeof(struct sockaddr_in);
+        error("ERROR: binding");
+
+    // Initializing client
+    clientlen = sizeof(client);
+    bzero(&client, clientlen);
+
+    // Server runs forever, I guess
     while (1)
     {
-        n = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&from, &fromlen);
+        n = recvfrom(sock, buf, MAXLINE, 0, (struct sockaddr *)&client, &clientlen);
         if (n < 0)
-            error("recvfrom");
-        write(1, "Received a datagram: ", 21);
-        write(1, buf, n);
-        n = sendto(sock, "Got your message\n", 17,
-                   0, (struct sockaddr *)&from, fromlen);
+            error("ERROR: recvfrom");
+        // write(1, "Received a datagram: ", 21);
+        // write(1, buf, n);
+        buf[n] = '\0';
+        printf("Received a datagram: %s\n", buf);
+
+        // ACK or REJ logic here:
+        // TODO: Check for all 4 error-cases!
+
+        // Sending response back to Client
+        // TODO: Change MSG_CONFIRM to to custom message
+        n = sendto(sock, (const char *)hello, strlen(hello),
+                   MSG_CONFIRM, (struct sockaddr *)&client, clientlen);
         if (n < 0)
-            error("sendto");
+            error("ERROR: sendto");
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
